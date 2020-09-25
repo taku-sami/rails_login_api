9a6560c5dd2e29481f9129288d42be9123b476f6
taku-sami <takuya062090@gmail.com>
2020-09-25 14:00:56 +0900

    user apiとlogin apiの作成

---
+++ db/migrate/20200925044719_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :mail
+      t.string :pwd
+      t.string :uid
+      t.string :token
+
+      t.timestamps
+    end
+    add_index :users, :token, unique: true
+  end
+end
